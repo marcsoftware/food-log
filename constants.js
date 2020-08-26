aa06372dfe037388f3d7d5ab5233b3208bb28a7e
marc <marcdavidmelcher@gmail.com>
2020-08-26 14:12:53 -0700

    added dumy file for importing constants

---
+++ constants.js
@@ -0,0 +1,5 @@
+var callback_url = `http://localhost:8080`;  // from fitbit.com app setting page
+
+var database_url='http://localhost:5000/demo'; // endpoint of java database
+
+//TODO url for code,authcode, endpoints
