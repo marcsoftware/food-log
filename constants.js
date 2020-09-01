297d91c8778754a2e3dad1beab8e26fc5ab30323
marc <marcdavidmelcher@gmail.com>
2020-09-01 16:16:11 -0700

    changed hardcoded value - goal

--- constants.js
+++ constants.js
@@ -1,8 +1,10 @@
-var callback_url = `http://localhost:8080`;  // from fitbit.com app setting page
+//var callback_url = `http://localhost:8080`;  // from fitbit.com app setting page
 
-var database_url='http://localhost:5000/demo'; // endpoint of java database
+//var database_url='http://localhost:5000/demo'; // endpoint of java database
 
 
-//var database_url="http://pantry-env.7zyk5zdmpf.us-east-1.elasticbeanstalk.com";
 
-//TODO url for code,authcode, endpoints----------------------------------
+//---------------------------------
+
+var database_url="http://pantry-env.7zyk5zdmpf.us-east-1.elasticbeanstalk.com";
+var callback_url =`http://hostreactapp.s3-website-us-east-1.amazonaws.com/`;
