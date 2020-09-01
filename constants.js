3be25ab88f31f89f8477dcaba0253beb9dc2ade6
marc <marcdavidmelcher@gmail.com>
2020-09-01 16:27:24 -0700

    added dummy header

--- constants.js
+++ constants.js
@@ -1,10 +1,12 @@
-//var callback_url = `http://localhost:8080`;  // from fitbit.com app setting page
+var callback_url = `http://localhost:8080`;  // from fitbit.com app setting page
 
-//var database_url='http://localhost:5000/demo'; // endpoint of java database
+var database_url='http://localhost:5000/demo'; // endpoint of java database
 
 
 
 //---------------------------------
 
+/*
 var database_url="http://pantry-env.7zyk5zdmpf.us-east-1.elasticbeanstalk.com";
 var callback_url =`http://hostreactapp.s3-website-us-east-1.amazonaws.com/`;
+*/
