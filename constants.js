7a76b36984e7cfed219b5a80e26901eb937e2cfc
marc <marcdavidmelcher@gmail.com>
2020-09-01 19:27:40 -0700

    added header

--- constants.js
+++ constants.js
@@ -3,7 +3,13 @@ var callback_url = `http://localhost:8080`;  // from fitbit.com app setting page
 var database_url='http://localhost:5000/demo'; // endpoint of java database
 
 
+function drawHeader(){
+    var handle=document.getElementById('header');
 
+    if(handle){
+        handle.innerHTML='<a href="/report.html">report</a> <a href="/foodlog.html">food log</a> <a href="/fitbit.html">fitbit</a> <a href="/scale.html">scale</a>'
+    }
+}
 //---------------------------------
 
 /*
