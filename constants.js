93312b1b1efc98da5a27cbd909efd5aacf9d6019
marc <marcdavidmelcher@gmail.com>
2020-09-04 17:34:54 -0700

    smarter constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
-var callback_url = `http://localhost:8080/login.html`;  // from fitbit.com app setting page
+var callback_url = ``;  // from fitbit.com app setting page
 
-var database_url='http://localhost:5000/demo'; // endpoint of java database
+var database_url=''; // endpoint of java database
 
+var client_id='';
 
 function drawHeader(){
     var handle=document.getElementById('header');
@@ -11,10 +12,32 @@ function drawHeader(){
     }
 }
 
-var client_id='22BVZC';
+
+
+
+
+function setConfig(){
+     
+
+     var pageURL = window.location;
+     
+          
+          
+
+     if(pageURL.toString().includes('localhost')){
+            callback_url = `http://localhost:8080/login.html`;  // from fitbit.com app setting page
+            database_url='http://localhost:5000/demo'; // endpoint of java database
+            client_id='22BVZC';
+
+     }else{
+            database_url="http://pantry-env.7zyk5zdmpf.us-east-1.elasticbeanstalk.com";
+            callback_url =`https://poundreport.com`;
+            client_id='22BWQ4';
+     }
+}
+
+setConfig();
+
 //---------------------------------
 
 
-//database_url="http://pantry-env.7zyk5zdmpf.us-east-1.elasticbeanstalk.com";
-//callback_url =`https://poundreport.com`;
-//client_id='22BWQ4';
