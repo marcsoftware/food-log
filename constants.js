4939879e7cac32cb45b3e3cbb64c3ee3a279c177
marc <marcdavidmelcher@gmail.com>
2020-09-06 16:33:48 -0700

    TESTED & WORKS added login hyperlink to header

--- constants.js
+++ constants.js
@@ -8,8 +8,10 @@ function drawHeader(){
     var handle=document.getElementById('header');
 
     if(handle){
-        handle.innerHTML='<a href="/index.html">food log</a> <a href="/report.html">report</a>  <a href="/fitbit.html">fitbit</a> <a id="hyperlink">login</a> <a href="/scale.html">scale</a>'
+        handle.innerHTML='<a href="/index.html">food log</a> <a href="/report.html">report</a>  <a href="/fitbit.html">fitbit</a> <a  href="" id="hyperlink">login</a> <a href="/scale.html">scale</a>'
     }
+
+        setConfig();
 }
 
 
@@ -53,7 +55,7 @@ function getUrl(){
 }
 
 
-setConfig();
+
 
 //---------------------------------
 
